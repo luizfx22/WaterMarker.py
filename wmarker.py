4803967c92a15cc1e41b0918487165b34a10ee7e
Luiz A F Gomes <luizgomes749@gmail.com>
2020-02-05 14:44:44 -0300

    Watermark working

--- wmarker.py
+++ wmarker.py
@@ -5,6 +5,7 @@ import json
 from sqlalchemy.sql import select
 import sqlalchemy as db
 from PIL import Image
+import time
 
 class WMarkr:
     def __init__(self):
@@ -42,7 +43,7 @@ class WMarkr:
             print(e)
             return
 
-    def addWatermarkImage(self, file_path):
+    def addWatermarkImage(self, file_path, output_path):
         base_image = Image.open(file_path)
         logo = Image.open(self.settings["watermark-image-location"])
         logo = logo.convert('RGBA')
@@ -53,11 +54,16 @@ class WMarkr:
         base_image.paste(logo, (imageWidth - logoWidth - 10, imageHeight - logoHeight - 10), logo)
         
         filename = file_path[file_path.rfind(os.path.sep) + 1:]
-        p = os.path.sep.join((self.settings["output-main-dir"], filename))
+        p = os.path.sep.join((output_path, filename))
+        try:
+            base_image.save(p)
+            print("\nImage saved successfully!", end="\r")
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-        base_image.save(p)
-
-    def addWatermarkThumbnail(self, file_path):
+    def addWatermarkThumbnail(self, file_path, output_path):
         base_image = Image.open(file_path)
         logo = Image.open(self.settings["watermark-image-location"])
         logo.thumbnail((110, 110), Image.ANTIALIAS)
@@ -70,18 +76,38 @@ class WMarkr:
                                 imageHeight - logoHeight - 5), logo)
 
         filename = file_path[file_path.rfind(os.path.sep) + 1:]
-        p = os.path.sep.join((self.settings["output-thumb-dir"], filename))
+        p = os.path.sep.join((output_path, filename))
 
-        base_image.save(p)
+        try:
+            base_image.save(p)
+            print("\nThumbnail saved successfully!", end="\r")
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
+bnchmrk_start = time.time()
 
 marker = WMarkr()
 conn = marker.connect()
 
 query = marker.query(conn)
 
+mark_ = len(query)
+
 for image_tuple in query:
-    
+
+    thumbnail_path = marker.settings["thumbnails-dir"] + image_tuple[0]
+    actual_image = marker.settings["input-dir"] + image_tuple[1]
+
+    if not marker.addWatermarkImage(actual_image, marker.settings["output-main-dir"]):
+        print(f"Skipped file {image_tuple[1]}!")
+    if not marker.addWatermarkThumbnail(thumbnail_path, marker.settings["output-thumb-dir"]):
+        print(f"Skipped thumbnail {image_tuple[0]}!")
+
+
+bnchmrk_end = time.time() - bnchmrk_start
+print(f"It took about {bnchmrk_end} seconds to mark {mark_} images!")
 
 # marker.addWatermarkThumbnail(marker.settings["thumbnails-dir"])
 # result = marker.query(marker.connect())
