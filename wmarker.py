2d3aaa74bf054b1609883eeb2280111591e54800
Luiz A F Gomes <luizgomes749@gmail.com>
2020-02-05 14:12:22 -0300

    Watermarking with PIL

--- wmarker.py
+++ wmarker.py
@@ -4,8 +4,7 @@ import os
 import json
 from sqlalchemy.sql import select
 import sqlalchemy as db
-import numpy as np
-import cv2
+from PIL import Image
 
 class WMarkr:
     def __init__(self):
@@ -20,7 +19,7 @@ class WMarkr:
         connection_string = f"{self.sql_settings['dialect']}://{self.sql_settings['username']}:{self.sql_settings['password']}@{self.sql_settings['host']}:{self.sql_settings['port']}/{self.sql_settings['database']}"
         try:
             engine = db.create_engine(connection_string)
-            connection = engine.connect()
+            self.connection = engine.connect()
             metadata = db.MetaData()
             table = db.Table(self.sql_settings["table"],
                 metadata, autoload=True, autoload_with=engine)
@@ -43,39 +42,47 @@ class WMarkr:
             print(e)
             return
 
-    def addWaterMark(self, file_path):
-        watermark = cv2.imread(self.settings["watermark-image-location"], cv2.IMREAD_UNCHANGED)
-        (wH, wW) = watermark.shape[:2]
+    def addWatermarkImage(self, file_path):
+        base_image = Image.open(file_path)
+        logo = Image.open(self.settings["watermark-image-location"])
+        logo = logo.convert('RGBA')
 
-        # r = 120 / float(wH)
-        # dim = (int(wH * r), wH)
+        imageWidth, imageHeight = base_image.width, base_image.height
+        logoWidth, logoHeight = logo.width, logo.height
         
-        # # Return the resized image
-        # watermark = cv2.resize(watermark, dim, interpolation=cv2.INTER_AREA)
-
-        (B, G, R, A) = cv2.split(watermark)
-        B = cv2.bitwise_and(B, B, mask=A)
-        G = cv2.bitwise_and(G, G, mask=A)
-        R = cv2.bitwise_and(R, R, mask=A)
-
-        watermark = cv2.merge([B, G, R, A])
-
-        image = cv2.imread(file_path)
-        (h, w) = image.shape[:2]
-        image = np.dstack([image, np.ones((h, w), dtype="uint8") * 255])
-
-        overlay = np.zeros((h, w, 4), dtype="uint8")
-        overlay[h - wH - 10:h - 10, w - wW - 10:w - 10] = watermark
-
-        output = image.copy()
-        cv2.addWeighted(overlay, 0.4, output, 1.0, 0, output)
+        base_image.paste(logo, (imageWidth - logoWidth - 10, imageHeight - logoHeight - 10), logo)
         
         filename = file_path[file_path.rfind(os.path.sep) + 1:]
-        p = os.path.sep.join((self.settings["output-dir"], filename))
-        cv2.imwrite(p, output)
+        p = os.path.sep.join((self.settings["output-main-dir"], filename))
+
+        base_image.save(p)
+
+    def addWatermarkThumbnail(self, file_path):
+        base_image = Image.open(file_path)
+        logo = Image.open(self.settings["watermark-image-location"])
+        logo.thumbnail((110, 110), Image.ANTIALIAS)
+        logo = logo.convert('RGBA')
+
+        imageWidth, imageHeight = base_image.width, base_image.height
+        logoWidth, logoHeight = logo.width, logo.height
+
+        base_image.paste(logo, (imageWidth - logoWidth - 5,
+                                imageHeight - logoHeight - 5), logo)
+
+        filename = file_path[file_path.rfind(os.path.sep) + 1:]
+        p = os.path.sep.join((self.settings["output-thumb-dir"], filename))
+
+        base_image.save(p)
 
 
 marker = WMarkr()
-marker.addWaterMark(marker.settings["input-dir"])
+conn = marker.connect()
+
+query = marker.query(conn)
+
+for image_tuple in query:
+    
+
+# marker.addWatermarkThumbnail(marker.settings["thumbnails-dir"])
 # result = marker.query(marker.connect())
 
