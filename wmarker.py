807d11631da091eaf462af859c6578cad3f1386a
Luiz A F Gomes <luizgomes749@gmail.com>
2020-02-05 10:22:12 -0300

    Reading json, added instructions too

--- wmarker.py
+++ wmarker.py
@@ -1,5 +1,30 @@
 # Import stuff
 import sqlalchemy as sql
+import cv2
 import json
 
+"""
+	Instructions:
+		1. Create a file called 'settings.json'
+		2. Write these lines in it:
+			{
+				"mysql": {
+					"url": "",
+					"user": "",
+					"password": "",
+					"db": ""
+				},
+				"input-dir": "",
+				"thumbnails-dir": "",
+				"output-dir": "/wmarker_results/"
+			}
+		3. Replace the empty spaces with your actual data, if you're not using MySQL leave it blank!
+		disclaimer: I didn't comitted settings.json due to sensitive data placed in it!
+"""
 
+with open('settings.json', 'r+') as settingsFile:
+	settings = json.load(settingsFile)
+	settingsFile.close()
+
+mysql = settings["mysql"]
+print(mysql)
